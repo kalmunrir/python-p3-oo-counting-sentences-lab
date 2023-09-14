6e3eb2c437a8e3dbe600691a34806c1a6bed6f6a
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2023-09-13 21:50:26 -0600

    finished lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  
+  def __init__(self, value = ""):
+    self.value = value
+
+  @property
+  def value(self):
+    return self._value
+  @value.setter
+  def value(self, value):
+    if isinstance(value, str):
+      self._value = value
+    else:
+      print("The value must be a string.")
+
+  def is_sentence(self):
+    return self.value.endswith('.')
+    
+  def is_question(self):
+    return self.value.endswith('?')
+  
+  def is_exclamation(self):
+    return self.value.endswith('!')
+  
+  def count_sentences(self):
+    if self.value.isspace() or self.value == '':
+      return 0
+    else:
+      sentence_list = self.value
+      sentence_list = sentence_list.replace('?', '.')
+      sentence_list = sentence_list.replace('!', '.')
+      sentence_list = sentence_list.split('.')
+      sentence_list.sort()
+      for i in range(sentence_list.count('')):
+        sentence_list.remove('')
+      return len(sentence_list)
